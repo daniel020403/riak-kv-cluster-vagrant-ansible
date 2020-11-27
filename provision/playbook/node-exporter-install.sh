bc1fb5b0a2261ccc3bdf45d332420d06e2bb438c
Daniel B. Garcia <dandan02.0403@gmail.com>
2020-11-27 10:07:36 +0800

    SNAPSHOT: 20201127-1007

--- provision/playbook/node-exporter-install.sh
+++ provision/playbook/node-exporter-install.sh
@@ -3,4 +3,3 @@
 tar -xzvf /tmp/$NODE_EXPORTER_PACKAGE -C /opt/
 chown -R root:root /opt/node_exporter-*.*-amd64
 ln -s /opt/node_exporter-*.*-amd64/node_exporter /usr/sbin/node_exporter
-nohup node_exporter &
